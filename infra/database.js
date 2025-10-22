95aee56bc38b180e56ba0cbeb522b3c29e61fddf
Wellington <sousa_wellington@outlook.com>
2025-10-22 19:52:06 -0300

    feat: add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,15 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
-  
-  try{
+
+  try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
-  } catch(error) {
-    console.error(error)
+  } catch (error) {
+    console.error(error);
   } finally {
     await client.end();
   }
@@ -24,3 +24,10 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return { ca: process.env.POSTGRES_CA };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+}
