c7a1fc58e7acc9e95182f0ea928dbdc48a3f3caa
Wellington <sousa_wellington@outlook.com>
2026-01-25 20:11:24 -0300

    chore: add ESLint configuration and scripts, update devDependencies

--- infra/database.js
+++ infra/database.js
@@ -2,6 +2,7 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   let client;
+  let x;
   try {
     client = await getNewClient();
     const result = await client.query(queryObject);
